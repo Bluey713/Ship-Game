16c739366da90929132f818441b09092dffd0e4f
Bluey Blue <blueyvuitton713@gmail.com>
2025-06-26 00:12:08 -0700

    added comments to the previous code and added the ship to the code

--- Alien Invasion.py
+++ Alien Invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
-from settings import Settings
+from settings import Settings       #This is importing the module we created
+from ship import Ship
 
 """The player controsl a rocket ship that appears on the bottom center of the screen.
 The player can move the ship left and right and shoots bullets with the space bar.
@@ -10,30 +11,33 @@ class AlienInvasion:
     """Overall class to manage game assets and behavior"""
     def __init__(self) -> None:
         """Initialized the game, and create game resources"""
-        pygame.init()
-        self.clock = pygame.time.Clock()
-        self.settings = Settings()
+        pygame.init()   #initialized all imporated modules instead of initializing each one we use
+        self.clock = pygame.time.Clock()    #Creates an object to help track time; which we'll then use to track fps
+        self.settings = Settings()  #we can now call the self.settings.whatever_method_is_in_Settings.
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
-        pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height)) #returns a "surface" = screen. see documentation
+        pygame.display.set_caption("Alien Invasion")    #sets the title on the open window
+
+        self.ship = Ship(self)
 
         #Set the background color.
-        self.bg_color = (230, 230, 230)
+        # self.bg_color = (200, 200, 200)   #commented this out for now since it seems like its not doing anything
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             #Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            for event in pygame.event.get():    #an event is any keyboard action or mouse click.
+                if event.type == pygame.QUIT:   #clicking on the x button is an event of QUIT.
                     sys.exit()
 
             #Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             #Make the most recently drawn screen visible.
-            pygame.display.flip()
-            self.clock.tick(60)     #sets the frame rate to 60fps
+            pygame.display.flip()   #refreshes the screen. documentation unclear maybe ask grok
+            self.clock.tick(60)     #sets the frame rate to 60 frames per "tick" = second.
 
-ai = AlienInvasion()
-ai.run_game()
+ai = AlienInvasion()    #initialize "ai" as our game
+ai.run_game()   #run the game by using the method which stores the loop
